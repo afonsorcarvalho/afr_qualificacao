b4b56e5ea6195889d3da52b578206e689b51a4bb
afonsorcarvalho <afonsorcarvalho@github.com>
2026-06-03 00:26:23 -0300

    chore(afr_qualificacao): bump to 16.0.5.10.0 (SO/OS sequence C[YY]-[MM]-NNNN)

--- __manifest__.py
+++ __manifest__.py
@@ -1,6 +1,6 @@
 {
     "name": "AFR Qualificação",
-    "version": "16.0.5.9.0",
+    "version": "16.0.5.10.0",
     "category": "Maintenance",
     "license": "LGPL-3",
     "author": "AFR Sistemas",
